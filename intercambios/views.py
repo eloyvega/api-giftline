bf63b6fb287706ff9d5a5cc4204fc71655cace67
Eloy Vega <eloy.vega@gmail.com>
2015-12-04 17:26:37 -0600

    Mixin de config de rest framework

--- intercambios/views.py
+++ intercambios/views.py
@@ -1,8 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, authentication, permissions
+
 from .models import Intercambio
 from .serializers import IntercambioSerializer
 
 
-class IntercambioViewSet(viewsets.ModelViewSet):
+class ConfigMixin(object):
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class IntercambioViewSet(ConfigMixin, viewsets.ModelViewSet):
     queryset = Intercambio.objects.order_by('fecha_creacion')
     serializer_class = IntercambioSerializer
