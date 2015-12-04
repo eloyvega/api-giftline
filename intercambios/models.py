7c5069914f70b6dad64431adbc31678b355ec4c6
Eloy Vega <eloy.vega@gmail.com>
2015-12-04 13:45:21 -0600

    Corrección de modelos

--- intercambios/models.py
+++ intercambios/models.py
@@ -6,32 +6,48 @@ class Intercambio(models.Model):
     STATUS_ABIERTO = 1
     STATUS_CERRADO = 2
     STATUS_ELIMINADO = 3
+    TIPO_PUBLICO = 1
+    TIPO_PRIVADO = 2
 
     STATUS_OPCIONES = (
         (STATUS_ABIERTO, 'Abierto'),
         (STATUS_CERRADO, 'Cerrado'),
         (STATUS_ELIMINADO, 'Eliminado'),
     )
+    TIPO_OPCIONES = (
+        (TIPO_PUBLICO, 'Público'),
+        (TIPO_PRIVADO, 'Privado'),
+    )
 
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField(blank=True)
     status = models.SmallIntegerField(choices=STATUS_OPCIONES, default=STATUS_ABIERTO)
+    tipo = models.SmallIntegerField(choices=TIPO_OPCIONES, default=TIPO_PRIVADO)
     fecha_creacion = models.DateField(auto_now_add=True)
     fecha_intercambio = models.DateField(blank=True, null=True)
-    amigos = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Participante')
+    amigos = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='Participante',
+        through_fields=('intercambio', 'usuario')
+    )
 
     def __str__(self):
         return self.nombre
 
 
 class Participante(models.Model):
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL)
     intercambio = models.ForeignKey(Intercambio)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL)
     es_admin = models.BooleanField(default=False)
-    amigo = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    amigo = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        related_name='amigo',
+    )
 
 
 class Invitacion(models.Model):
-    email_invitado = models.EmailField(max_length=254)
     intercambio = models.ForeignKey(Intercambio)
+    email_invitado = models.EmailField(max_length=254)
     mensaje = models.TextField(blank=True)
